a5865ce2a04f54af2751c14823b4e33636faaa50
gincio <hubokendzior@gmail.com>
2018-03-16 19:23:06 +0100

    Front-end part 1
    - nowa strona logowania
    - poprawiony formularz rejestracji użytkowników (klasy bootstrapowe, grid bootstrapowy, responsywność, poprawki w kodzie)
    - dodane logo i zmodyfikowany header w panelu admina
    - dodany favicon
    - zmodyfikowany tytuł strony (html <title>)

--- resources/views/admin/rejestracja.blade.php
+++ resources/views/admin/rejestracja.blade.php
@@ -5,26 +5,24 @@ Rejestracja
 @endsection
 
 @section('head')
-<style  type="text/css">
-.control-group
-{
-      text-align: center;
-}
-.form-group
-{
-	text-align: center;
-	margin-bottom: 0px;
-}
-.form-group select
-{
-    margin: 0 auto;
-	width: 15%;
-}
-.container
-{
-	margin-bottom: 10px;
-}
-</style>
+	<style  type="text/css">
+		.control-group
+		{
+			text-align: center;
+		}
+		.form-group
+		{
+			text-align: center;
+			margin-bottom: 0px;
+		}
+		.form-group select
+		{
+			margin: 0 auto;
+		}
+		.container {
+			margin-bottom: 10px;
+		}
+	</style>
 @endsection
 
 @section('content')
@@ -39,13 +37,13 @@ Rejestracja
 	    }
 	?>
 
-<div class="col-sm-10" >
+<div class="col-sx-12 col-sm-8 col-md-10" >
     <div class="panel panel-default">
        <div class="panel-heading">
                 Rejestracja nowego profilu
         </div>
         <div class="panel-body">
-		<form class="form-horizontal" action='' method="POST">
+		<form class="form" role="form" action='' method="POST">
 		  <fieldset >
 		  	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 		    <?php
@@ -53,9 +51,10 @@ Rejestracja
 				$error = session()->get('error');
 				echo $error;
 			?>
+		  <div class="col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2">
 		    <div class="form-group">
-			    <label>Funkcja</label>
-			    <select class="form-control" name="funkcja" onchange="yesnoCheck(this);">
+			    <label for="funkcja">Funkcja</label>
+			    <select class="form-control" name="funkcja" id="funkcja" onchange="yesnoCheck(this);">
 			      <option value="lekarz">Lekarz</option>
 			      <option value="ordynator">Ordynator</option>
 			      <option value="pacjent">Pacjent</option>
@@ -63,8 +62,8 @@ Rejestracja
 			    <p class="help-block">Wybierz jaką funkcję ma spełniać nowy użytkownik</p>
 			</div>			
 			<div class="form-group" id="gdyPacjent" style="display: none;">
-			    <label for="exampleFormControlSelect1">Lekarz</label>
-			    <select class="form-control" name="patientsDoctor" id="exampleFormControlSelect1">
+			    <label for="patientsDoctor">Lekarz</label>
+			    <select class="form-control" name="patientsDoctor" id="patientsDoctor">
 			      @if(sizeof($lekarze) == 0)
 			      	<option value="none">Brak lekarzy</option>
 			      @endif
@@ -72,79 +71,80 @@ Rejestracja
 					 <option value="{{ $test->id }}">{{ $test->imie }} {{ $test->nazwisko }}</option>
 				  @endforeach
 			    </select>
-			    <p class="help-block">Wybierz lekarza którego podwładnym ma być nowy pacjent</p>
+			    <p class="help-block">Wybierz lekarza, do którego przypisany będzie nowy pacjent</p>
 			</div>
 
-		    <div class="control-group">
-		      <label class="control-label"  for="username">Imię</label>
+		    <div class="form-group">
+		      <label class="control-label"  for="forename">Imię</label>
 		      <div class="controls">
-		        <input type="text" name="forename" placeholder="" class="input-xlarge">
+		        <input type="text" name="forename" id="forename" placeholder="" class="form-control">
 		        <p class="help-block">Wprowadź imię</p>
 		      </div>
 		    </div>
 		    <div class="control-group">
-		      <label class="control-label"  for="username">Nazwisko</label>
+		      <label class="control-label" for="surname">Nazwisko</label>
 		      <div class="controls">
-		        <input type="text" name="surname" placeholder="" class="input-xlarge">
+		        <input type="text" name="surname" id="surname" placeholder="" class="form-control">
 		        <p class="help-block">Wprowadź nazwisko</p>
 		      </div>
 		    </div>
 		    <div class="form-group" id="gdyPacjent1" style="display: none;">
-			    <label for="exampleFormControlSelect1">Płeć</label>
-			    <select class="form-control" name="gender" id="exampleFormControlSelect1">
+			    <label for="gender">Płeć</label>
+			    <select class="form-control" name="gender" id="gender">
 			      <option value="Kobieta">Kobieta</option>
 			      <option value="Mężczyzna">Mężczyzna</option>
 			    </select>
 			    <p class="help-block">Wybierz płeć </p>
 			</div>
-			<div class="control-group">
-		      <label class="control-label"  for="username">Data urodzenia</label>
+			<div class="form-group">
+		      <label class="control-label"  for="dateOfBirth">Data urodzenia</label>
 		      <div class="controls">
-		          <input type="date" name="dateOfBirth" placeholder="" class="input-xlarge">
+		          <input type="date" name="dateOfBirth" placeholder="" class="form-control">
 		        <p class="help-block">Wprowadź datę urodzenia</p>
 		      </div>
 		    </div>
-		    <div class="control-group">
-		      <label class="control-label"  for="username">PESEL</label>
+		    <div class="form-group">
+		      <label class="control-label"  for="pesel">PESEL</label>
 		      <div class="controls">
-		          <input type="text" name="pesel" placeholder="" class="input-xlarge">
+		          <input type="text" name="pesel" placeholder="" class="form-control">
 		        <p class="help-block">Wprowadź numer PESEL</p>
 		      </div>
 		    </div>
-		    <div class="control-group" id="gdyPacjent5" style="display: none;">
-		      <label class="control-label"  for="username">Zdjęcie rentgentowskie</label>
+		    <div class="form-group" id="gdyPacjent5" style="display: none;">
+		      <label class="control-label"  for="photo">Zdjęcie rentgentowskie</label>
 		      <div class="controls">
-		          <input type="text" name="pesel" placeholder="" class="input-xlarge">
+		          <input type="file" name="photo" placeholder=""  class="form-control-file">
 		        <p class="help-block">Wybierz zdjęcie rentgentowskie pacjenta</p>
 		      </div>
 		    </div>
-		    <div class="control-group" id="gdyPacjent2">
+		    <div class="form-group" id="gdyPacjent2">
 		      <label class="control-label"  for="username">Login</label>
 		      <div class="controls">
-		        <input type="text" name="username" placeholder="" class="input-xlarge">
+		        <input type="text" name="username" placeholder="" class="form-control">
 		        <p class="help-block">Wprowadź login (bez spacji)</p>
 		      </div>
 		    </div>
-		    <div class="control-group" id="gdyPacjent3">
+		    <div class="form-group" id="gdyPacjent3">
 		      <label class="control-label" for="password">Hasło</label>
 		      <div class="controls">
-		        <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+		        <input type="password" id="password" name="password" placeholder="" class="form-control">
 		        <p class="help-block">Hasło powinno się składać z co najmniej 4 znaków</p>
 		      </div>
 		    </div>
 		 
-		    <div class="control-group" id="gdyPacjent4">
+		    <div class="form-group" id="gdyPacjent4">
 		      <label class="control-label"  for="password_confirm">Potwierdź hasło</label>
 		      <div class="controls">
-		        <input type="password" id="password_confirm" name="password_confirm" placeholder="" class="input-xlarge">
+		        <input type="password" id="password_confirm" name="password_confirm" placeholder="" class="form-control">
 		        <p class="help-block">Wprowadzone hasła muszą być identyczne</p>
 		      </div>
 		    </div>
-		    <div class="control-group">
+		    <div class="form-group">
 		      <div class="controls">
 		        <button class="btn btn-success">Zarejestruj</button>
 		      </div>
 		    </div>
+		  </div>
 		  </fieldset>
 		</form>
 		</div>
