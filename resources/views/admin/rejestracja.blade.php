7f7be829603576980b3378b06a498ddf7062e367
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-03-18 15:35:19 +0100

    Zamiana phpowych ifów na blade'owe ify

--- resources/views/admin/rejestracja.blade.php
+++ resources/views/admin/rejestracja.blade.php
@@ -26,143 +26,143 @@
 @endsection
 
 @section('content')
-      <div class="col-sx-12 col-sm-8 col-md-10">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          Rejestracja nowego profilu
-        </div>
-        <div class="panel-body">
-          <form class="form" role="form" action='' method="POST">
-            <fieldset>
-              <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <?php
-                // Wyświetlanie błędów
-                $error = session()->get('error');
-                if ($error != '')
-                    echo '<div class="alert alert-danger alert-dismissible fade in error"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' . $error . '</div>';
-                ?>
-              <div class="col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2">
-                <div class="form-group">
-                  <label for="funkcja">Funkcja</label>
-                  <select class="form-control" name="funkcja" id="funkcja" onchange="yesnoCheck(this);">
-                    <option value="lekarz">Lekarz</option>
-                    <option value="ordynator">Ordynator</option>
-                    <option value="pacjent">Pacjent</option>
-                  </select>
-                  <p class="help-block">Wybierz jaką funkcję ma spełniać nowy użytkownik</p>
-                </div>
-                <div class="form-group" id="gdyPacjent" style="display: none;">
-                  <label for="patientsDoctor">Lekarz</label>
-                  <select class="form-control" name="patientsDoctor" id="patientsDoctor">
-                    @if(sizeof($lekarze) == 0)
-                      <option value="none">Brak lekarzy</option>
-                    @endif
-                    @foreach($lekarze as $test)
-                      <option value="{{ $test->id }}">{{ $test->imie }} {{ $test->nazwisko }}</option>
-                    @endforeach
-                  </select>
-                  <p class="help-block">Wybierz lekarza, do którego przypisany będzie nowy pacjent</p>
-                </div>
+  <div class="col-sx-12 col-sm-8 col-md-10">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        Rejestracja nowego profilu
+      </div>
+      <div class="panel-body">
+        <form class="form" role="form" action='' method="POST">
+          <fieldset>
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                <div class="form-group">
-                  <label class="control-label" for="forename">Imię</label>
-                  <div class="controls">
-                    <input type="text" name="forename" id="forename" placeholder="" class="form-control">
-                    <p class="help-block">Wprowadź imię</p>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="control-label" for="surname">Nazwisko</label>
-                  <div class="controls">
-                    <input type="text" name="surname" id="surname" placeholder="" class="form-control">
-                    <p class="help-block">Wprowadź nazwisko</p>
-                  </div>
-                </div>
-                <div class="form-group" id="gdyPacjent1" style="display: none;">
-                  <label for="gender">Płeć</label>
-                  <select class="form-control" name="gender" id="gender">
-                    <option value="Kobieta">Kobieta</option>
-                    <option value="Mężczyzna">Mężczyzna</option>
-                  </select>
-                  <p class="help-block">Wybierz płeć </p>
-                </div>
-                <div class="form-group">
-                  <label class="control-label" for="dateOfBirth">Data urodzenia</label>
-                  <div class="controls">
-                    <input type="date" name="dateOfBirth" placeholder="" class="form-control">
-                    <p class="help-block">Wprowadź datę urodzenia</p>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="control-label" for="pesel">PESEL</label>
-                  <div class="controls">
-                    <input type="text" name="pesel" placeholder="" class="form-control">
-                    <p class="help-block">Wprowadź numer PESEL</p>
-                  </div>
-                </div>
-                <div class="form-group" id="gdyPacjent5" style="display: none;">
-                  <label class="control-label" for="photo">Zdjęcie rentgentowskie</label>
-                  <div class="controls">
-                    <input type="file" name="photo" placeholder="" class="form-control-file">
-                    <p class="help-block">Wybierz zdjęcie rentgentowskie pacjenta</p>
-                  </div>
-                </div>
-                <div class="form-group" id="gdyPacjent2">
-                  <label class="control-label" for="username">Login</label>
-                  <div class="controls">
-                    <input type="text" name="username" placeholder="" class="form-control">
-                    <p class="help-block">Wprowadź login (bez spacji)</p>
-                  </div>
-                </div>
-                <div class="form-group" id="gdyPacjent3">
-                  <label class="control-label" for="password">Hasło</label>
-                  <div class="controls">
-                    <input type="password" id="password" name="password" placeholder="" class="form-control">
-                    <p class="help-block">Hasło powinno się składać z co najmniej 4 znaków</p>
-                  </div>
-                </div>
+            @if (session()->get('error'))
+              <div class="alert alert-danger alert-dismissible fade in error">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>{{ session()->get('error') }}
+              </div>';
+            @endif
+            <div class="col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2">
+              <div class="form-group">
+                <label for="funkcja">Funkcja</label>
+                <select class="form-control" name="funkcja" id="funkcja" onchange="yesnoCheck(this);">
+                  <option value="lekarz">Lekarz</option>
+                  <option value="ordynator">Ordynator</option>
+                  <option value="pacjent">Pacjent</option>
+                </select>
+                <p class="help-block">Wybierz jaką funkcję ma spełniać nowy użytkownik</p>
+              </div>
+              <div class="form-group" id="gdyPacjent" style="display: none;">
+                <label for="patientsDoctor">Lekarz</label>
+                <select class="form-control" name="patientsDoctor" id="patientsDoctor">
+                  @if(sizeof($lekarze) == 0)
+                    <option value="none">Brak lekarzy</option>
+                  @endif
+                  @foreach($lekarze as $test)
+                    <option value="{{ $test->id }}">{{ $test->imie }} {{ $test->nazwisko }}</option>
+                  @endforeach
+                </select>
+                <p class="help-block">Wybierz lekarza, do którego przypisany będzie nowy pacjent</p>
+              </div>
 
-                <div class="form-group" id="gdyPacjent4">
-                  <label class="control-label" for="password_confirm">Potwierdź hasło</label>
-                  <div class="controls">
-                    <input type="password" id="password_confirm" name="password_confirm" placeholder=""
-                           class="form-control">
-                    <p class="help-block">Wprowadzone hasła muszą być identyczne</p>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="controls">
-                    <button class="btn btn-success">Zarejestruj</button>
-                  </div>
+              <div class="form-group">
+                <label class="control-label" for="forename">Imię</label>
+                <div class="controls">
+                  <input type="text" name="forename" id="forename" placeholder="" class="form-control">
+                  <p class="help-block">Wprowadź imię</p>
                 </div>
               </div>
-            </fieldset>
-          </form>
-        </div>
+              <div class="form-group">
+                <label class="control-label" for="surname">Nazwisko</label>
+                <div class="controls">
+                  <input type="text" name="surname" id="surname" placeholder="" class="form-control">
+                  <p class="help-block">Wprowadź nazwisko</p>
+                </div>
+              </div>
+              <div class="form-group" id="gdyPacjent1" style="display: none;">
+                <label for="gender">Płeć</label>
+                <select class="form-control" name="gender" id="gender">
+                  <option value="Kobieta">Kobieta</option>
+                  <option value="Mężczyzna">Mężczyzna</option>
+                </select>
+                <p class="help-block">Wybierz płeć </p>
+              </div>
+              <div class="form-group">
+                <label class="control-label" for="dateOfBirth">Data urodzenia</label>
+                <div class="controls">
+                  <input type="date" name="dateOfBirth" placeholder="" class="form-control">
+                  <p class="help-block">Wprowadź datę urodzenia</p>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label" for="pesel">PESEL</label>
+                <div class="controls">
+                  <input type="text" name="pesel" placeholder="" class="form-control">
+                  <p class="help-block">Wprowadź numer PESEL</p>
+                </div>
+              </div>
+              <div class="form-group" id="gdyPacjent5" style="display: none;">
+                <label class="control-label" for="photo">Zdjęcie rentgentowskie</label>
+                <div class="controls">
+                  <input type="file" name="photo" placeholder="" class="form-control-file">
+                  <p class="help-block">Wybierz zdjęcie rentgentowskie pacjenta</p>
+                </div>
+              </div>
+              <div class="form-group" id="gdyPacjent2">
+                <label class="control-label" for="username">Login</label>
+                <div class="controls">
+                  <input type="text" name="username" placeholder="" class="form-control">
+                  <p class="help-block">Wprowadź login (bez spacji)</p>
+                </div>
+              </div>
+              <div class="form-group" id="gdyPacjent3">
+                <label class="control-label" for="password">Hasło</label>
+                <div class="controls">
+                  <input type="password" id="password" name="password" placeholder="" class="form-control">
+                  <p class="help-block">Hasło powinno się składać z co najmniej 4 znaków</p>
+                </div>
+              </div>
+
+              <div class="form-group" id="gdyPacjent4">
+                <label class="control-label" for="password_confirm">Potwierdź hasło</label>
+                <div class="controls">
+                  <input type="password" id="password_confirm" name="password_confirm" placeholder=""
+                         class="form-control">
+                  <p class="help-block">Wprowadzone hasła muszą być identyczne</p>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="controls">
+                  <button class="btn btn-success">Zarejestruj</button>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+        </form>
       </div>
     </div>
+  </div>
 
 
 
-    <script>
-      function yesnoCheck(that) {
-        if (that.value == 'pacjent') {
-          document.getElementById('gdyPacjent').style.display = 'block';
-          document.getElementById('gdyPacjent1').style.display = 'block';
-          document.getElementById('gdyPacjent2').style.display = 'none';
-          document.getElementById('gdyPacjent3').style.display = 'none';
-          document.getElementById('gdyPacjent4').style.display = 'none';
-          document.getElementById('gdyPacjent5').style.display = 'block';
+  <script>
+    function yesnoCheck(that) {
+      if (that.value == 'pacjent') {
+        document.getElementById('gdyPacjent').style.display = 'block';
+        document.getElementById('gdyPacjent1').style.display = 'block';
+        document.getElementById('gdyPacjent2').style.display = 'none';
+        document.getElementById('gdyPacjent3').style.display = 'none';
+        document.getElementById('gdyPacjent4').style.display = 'none';
+        document.getElementById('gdyPacjent5').style.display = 'block';
 
-        } else {
-          document.getElementById('gdyPacjent').style.display = 'none';
-          document.getElementById('gdyPacjent1').style.display = 'none';
-          document.getElementById('gdyPacjent2').style.display = 'block';
-          document.getElementById('gdyPacjent3').style.display = 'block';
-          document.getElementById('gdyPacjent4').style.display = 'block';
-          document.getElementById('gdyPacjent5').style.display = 'none';
-        }
+      } else {
+        document.getElementById('gdyPacjent').style.display = 'none';
+        document.getElementById('gdyPacjent1').style.display = 'none';
+        document.getElementById('gdyPacjent2').style.display = 'block';
+        document.getElementById('gdyPacjent3').style.display = 'block';
+        document.getElementById('gdyPacjent4').style.display = 'block';
+        document.getElementById('gdyPacjent5').style.display = 'none';
       }
+    }
 
-    </script>
+  </script>
 @endsection
